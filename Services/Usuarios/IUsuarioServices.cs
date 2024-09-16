8c9caa949e3f11ccff3ffdf625b07c4476c25825
RonaldoGM <rg23006@esfe.agape.edu.sv>
2024-09-16 11:49:17 -0600

    Creacion de servicios para Usuario

---
+++ Services/Usuarios/IUsuarioServices.cs
@@ -0,0 +1,13 @@
+﻿using GestionBibliotecaAPI.DTOs;
+
+namespace GestionBibliotecaAPI.Services.Usuarios
+{
+    public interface IUsuarioServices
+    {
+        Task<int> PostUsuario(UsuarioRequest usuario);
+        Task<List<UsuarioResponse>> GetUsuarios();
+        Task<UsuarioResponse> GetUsuario(int usuarioId);
+        Task<int> PutUsuario(int usuarioId, UsuarioRequest usuario);
+        Task<int> DeleteUsuario(int usuarioId);
+    }
+}
